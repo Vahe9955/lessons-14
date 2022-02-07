94d5565fdbf07278dd852e83b5b2d9a80ba7726a
HP <HP@DESKTOP-3V6FH78>
2022-02-07 23:36:26 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace lessons_14
+{
+    class Butterfly
+    {
+        public string name;
+
+        public void Fly()
+        {
+
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine($"{name} Fly!");
+        }
+        
+    }
+    class Program
+    {
+        public static void swap(int a, int b)
+        {
+            a = a - b;
+            b = a + b;
+            a = b - a;
+
+            
+        }
+        static void Main()
+        {
+            Butterfly mahon = new Butterfly();
+            mahon.name = "Admiral";
+            mahon.Fly();
+            mahon.name = "dfghj";
+            mahon.Fly();
+
+            Butterfly b2 = new Butterfly
+            {
+                name = "Vardan"
+            };
+
+            b2.Fly();
+
+
+
+
+        }
+    }
+} 
